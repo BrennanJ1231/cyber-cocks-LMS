92424cf7ff8e215a8122a0c23c58d7b40bc8d820
dakotash <dakotash@email.sc.edu>
2023-03-13 21:59:49 -0400

    Added the rest of the classes

---
+++ RegisteredUser.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class RegisteredUser extends User {
+
+    protected ArrayList <Course> currentCourse;
+    protected ArrayList <Progress> courseProgress;
+
+    public RegisteredUser(String username, String name, String email, int age){
+
+    }
+
+    public void addFavorite(){
+
+    }
+    public void courseEnrolled(){
+
+    }
+    public void getCourse(){
+
+    }
+    public void registerForCourse(){
+
+    }
+    public void unregisterForCourse(){
+
+    }
+    public void addRating(){
+
+    }
+    public boolean showComment(int age){
+        //@TODO add return type
+    }
+
+}
