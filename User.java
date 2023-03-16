f5aacb7a0dbe9ad5e2767c778f40c9757882d59a
Mason Loeb <mloeb05@gmail.com>
2023-03-16 12:13:56 -0400

    Added to User and UI

--- User.java
+++ User.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class User {
     protected String userID;
     protected String username;
+    protected String type;
     protected String firstName;
     protected String lastName;
     protected String password;
@@ -14,11 +15,18 @@ public class User {
         this.password = password;
         this.email = email;
         this.birthday = birthday;
+        this.type = type;
     }
     public String getUserID() {
         return this.userID;
     }
-    public String getPassword( {
+    public String getPassword() {
         return this.password;
-    })
+    }
+    public String getFirstName() {
+        return this.firstName;
+    }
+    public String getType() {
+        return this.type;
+    }
 }
