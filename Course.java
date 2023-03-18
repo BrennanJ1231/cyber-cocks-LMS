6f66337e58549fe5a2f13ab2a367b8c73fab370d
dakotash <dakotash@email.sc.edu>
2023-03-17 20:52:33 -0400

    added some more method stubs

--- Course.java
+++ Course.java
@@ -27,12 +27,12 @@ public class Course {
         this.comments =  comments;
     }
     public void addModule(Module module) {
-
+        modules.add(module);
     }
     public void removeModule(Module modul) {
-
+    modules.remove(modul);
     }
     public void addComments(ArrayList<Comment> comments) {
-
+        comments.addAll(comments);
     }
 }
