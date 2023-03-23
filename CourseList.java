98a406e8e0093991986bac97aab151a89750ef35
dakotash <dakotash@email.sc.edu>
2023-03-23 12:33:18 -0400

    testing

--- CourseList.java
+++ CourseList.java
@@ -2,45 +2,12 @@ import java.util.ArrayList;
 import java.util.UUID;
 
 public class CourseList {
-    
-    private ArrayList <Course> courses;
-    private static CourseList courseList;
 
-    public CourseList() {
-        courses = DataLoader.loadCourses();
-    }
-
-    public static CourseList getInstance() {
-		if (courseList == null) {
-			courseList = new CourseList();
-		}
-		return courseList;
-	}
-    public void addCourse(Course course){
-        if(courses == null) 
-            return;
-        courses.add(course);
-    }
-    public ArrayList<Course> getAll(){
-        return courses;
-    }
-    public void deleteCourse(Course course){
-        for(int i = 0; i <courses.size(); i++) {
-            if(courses.get(i).uuid.equals(course.uuid)) {
-                courses.remove(i);
-            }
-        }
-    }
-    public void save(){
-        DataWriter.saveCourse(DataWriter.COURSES_FILE_NAME);
-    }
-
-    public Course findCourse(String name) {
-        for (int i=0; i<courses.size();i++) {
-            if (courses.get(i).name.equalsIgnoreCase(name)) {
-                return courses.get(i);
-            }
-        }
-        return null;
-    }
+/*
+ * instance var
+ */
+private ArrayList<Course> courses;
+private static CourseList courseList;
 }
+
+
