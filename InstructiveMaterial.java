e06355ca19851c2783285b473482cb07bc7607d9
dakotash <dakotash@email.sc.edu>
2023-03-28 12:40:22 -0400

    Instructive Material

--- InstructiveMaterial.java
+++ InstructiveMaterial.java
@@ -1,6 +1,15 @@
+/**
+ * Represents instructive materials for a course or module  
+ */
 public class InstructiveMaterial {
     protected String name;
     protected String content;
+
+/**
+Constructor for InstructiveMaterial class.
+@param name the name of the instructive material.
+@param content the content of the instructive material.
+*/
     public InstructiveMaterial(String name, String content) {
         this.name = name;
         this.content = content;
