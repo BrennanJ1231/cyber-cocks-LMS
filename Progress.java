e79212ee8e8954d1827af6b84cc0b6596cfb2f06
dakotash <dakotash@email.sc.edu>
2023-03-28 12:51:36 -0400

    progress

--- Progress.java
+++ Progress.java
@@ -1,10 +1,19 @@
+/**
+
+The Progress class represents the progress of a user in a course or module and calculate the progress.
+*/
 public class Progress {
 
     protected double courseProgress;
     protected Course course;
     protected double moduleProgress;
     protected Module module;
-
+    
+    /** 
+     * This methid calculates the progress of the user
+    @param module The module object to calculate the progress of
+    @return The progress of the module as a decimal value 
+    */
     public double calculateModuleProgress(Module module){
         double numAssignment = module.test.size();
         double completed = 0;
