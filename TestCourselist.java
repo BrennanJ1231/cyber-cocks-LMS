5f8aa87f90b7143f744d5d4a29d810053a5cfca8
dakotash <dakotash@email.sc.edu>
2023-04-06 22:02:32 -0400

    test

--- TestCourselist.java
+++ TestCourselist.java
@@ -1,21 +1,30 @@
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
+import java.util.UUID;
 
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.jupiter.api.Test;
-
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import java.util.ArrayList;
 
+//{"comments":[{"comments":[],"author":"89253f65-4606-4f10-93a1-0a5bc9966247","datePosted":"11\/09\/2022","content":"fun"}],"name":"intro to python","rating":0.0,"description":"python introduction","language":"PYTHON","UUID":"d9057ce7-36c9-4080-94fc-77235f400c4a","modules":[{"material":[{"name":"for loops","content":"for loops"}],"assignment":[{"name":"for loops","questions":[{"name":"what is a for loop","choices":["a loop","not a loop"],"correctAnswer":"a loop"}]}],"description":"for and while loops","title":"loops"}]}]
 public class TestCourselist {
+
+    ArrayList<Course> courses =new ArrayList<>();
+    CourseList CL = new CourseList(); 
+    Course courseT;
+    UUID uuid = UUID.randomUUID();
+
+    @BeforeEach
+    public void setup() {
+        courseT = new Course(null, null, null,uuid, null, null);
+        CL.addCourse(courseT);
+    }   
  
-    //if it is returning a cours list object rather than null.
+//if it is returning a cours list object rather than null.
 @Test
 public void testGetInstance(){
     CourseList clTest = CourseList.getInstance();
@@ -25,42 +34,43 @@ public void testGetInstance(){
 //Sees if the course is adding to the list or not
 @Test
 public void addCourse(){
-    ArrayList<Course> courses = new ArrayList<>();
-    CourseList CL = new CourseList(); 
-    CL.addCourse(new Course(null, null, null, null, null, null));
     assertEquals(1,CL.getAll().size());
 }
 
-
-
-@Test
-public void testDeleteCourse() {
-    CourseList CL = new CourseList();
-    Course course = new Course("Test Course", "123", null, null, null, null);
-    CL.addCourse(course);
-    CL.deleteCourse(course);
-    ArrayList<Course> allCourses = CL.getAll();
-    Assertions.assertTrue(allCourses.isEmpty());
-}
-
+//Testing to see if it returns courses instaed of null
 @Test
 public void testGetAll(){
-
+assertNotNull(CL.getAll());
 }
 
+//testing to see if the course we add gets deleted 
 @Test 
 public void testDeletingCourse(){
-
+CL.deleteCourse(courseT);
+assertEquals(0, CL.getAll().size());
 }
 
+//This makses sure you can save and load from the JSON file
 @Test
 public void testSave(){
-    
+    DataWriter.saveCourse();
+    ArrayList <Course> courseTest = DataLoader.loadCourses();
+    assertNotNull(courseTest);
 }
 
+//Test if the exact data added tothe JSON fileis saved 
 @Test
-public Course testFindCourse(String name) {
+public void testSaveData(){
+    int jFileSize = DataLoader.loadCourses().size();
+    DataWriter.saveCourse();
+    assertNotEquals(jFileSize, DataLoader.loadCourses().size());
 
+}
+
+//Test to see if you can find a course by a name
+@Test
+public void testFindCourse() {
+assertNotNull(CL.findCourse("intro to python"));;
 }
 
 
